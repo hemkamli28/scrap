ad61fd9d753feecdc3032653dc51abdab1b73441
hemkamli28 <hemk2810@gmail.com>
2024-03-05 01:25:39 +0530

    a

--- src/routes.ts
+++ src/routes.ts
@@ -6,71 +6,49 @@ export const router = createPlaywrightRouter();
 
 router.addDefaultHandler(async ({ page, log }) => {
     log.info(`enqueueing new URLs`);
-    // await page.waitForSelector('.close a');
-    await page.screenshot({ path: 'screenshotstart.png' });
-
-    // await page.locator('//*[@id="webklipper-publisher-widget-container-notification-close-div"]').click();
-    // await page.locator('//*[@id="webklipper-publisher-widget-container-notification-close-div"]').click();
-    // await page.waitForTimeout(5000);
-    // class="page-section sticky-scroll topSection appendBottom40"
-    // xyz.click();
-    // await page.screenshot({ path: 'screenshotFrom.png' });
-
+    
     log.info('fetched btn');
     log.info("clicking...")
+    await page.waitForTimeout(3860);
+    await page.screenshot({ path: 'screenshotstart.png' });
+    await page.mouse.click(100, 200);
+    console.log('done');
 
-    // await page.waitForSelector('#second-img')
-    // await page.locator('//*[@id="top-banner"]/div[2]/div/div/div/div[2]/div[1]/div[1]')
-    // const banner = await page.waitForSelector('//*[@id="top-banner"]/div[2]')
-    await page.waitForTimeout(1000);
-    // .click();
-    await  page.mouse.click(100, 200)
-    // await page.waitForTimeout(1000);
-console.log('done');
-
-    await page.waitForTimeout(1000);
-
+    await page.waitForSelector('#top-banner > div.minContainer > div > div > div > div.fsw > div.fsw_inner.returnPersuasion > div.flt_fsw_inputBox.searchCity.inactiveWidget');
     await page.locator('//*[@id="top-banner"]/div[2]/div/div/div/div[2]/div[1]/div[1]').click();
-    await page.getByPlaceholder('From').fill('AMD');
-    console.log('done2');
+    await page.getByPlaceholder('From').fill('BOM');
     await page.waitForTimeout(1000);
-    await page.keyboard.press('ArrowDown')
-    // await page.keyboard.press('Enter')
-
+    await page.keyboard.press('ArrowDown');
     await page.keyboard.press('Tab')
+
     await page.getByPlaceholder('To').fill('DXB');
     await page.waitForTimeout(1000);
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('Tab')
 
-    await page.keyboard.press('ArrowDown')
-    await page.keyboard.press('Enter')
-    // await page.locator('//*[@id="react-autowhatever-1-section-0-item-0"]/div/div[1]/p[1]').click();
+    // clusterViewPrice
     await page.locator('//*[@id="top-banner"]/div[2]/div/div/div/div[2]/div[1]/div[3]/div[1]/div/div/div/div[2]/div/div[2]/div[1]/div[3]/div[3]/div[6]/div').click()
     await page.locator('//*[@id="top-banner"]/div[2]/div/div/div/div[2]/p/a').click()
-    await page.waitForTimeout(5000);
-    await  page.mouse.click(100, 200)
-    await page.waitForTimeout(15000);
-    await page.keyboard.down('End').then(()=>{
-        console.log("donescroll");
+
+    await page.waitForSelector('#root > div > div:nth-child(2) > div.flightBody > div.overlay > div > div > div.makeFlex.hrtlCenter.right > button')
+    await page.screenshot({ path: 'banner.png' });
+    
+    await page.locator('//*[@id="root"]/div/div[2]/div[2]/div[2]/div/div/div[3]/button').click();
+
+    await page.waitForTimeout(1000);
+
+    for (let i = 0; i < 5; i++) {
+        await page.keyboard.press('End');
         
-    });
+    }
 
-
-    
-    
+    await page.waitForTimeout(3000);
     const categoryTexts = await page.$$eval('.airlineName', (els) => {
-        // Extract text content from the actor cards
         return els.map((el) => el.textContent);
     });
     categoryTexts.forEach((text, i) => {
         console.log(`Airline: ${i + 1}: ${text}\n`);
-    });    
-    //*[@id="listing-id"]/div/div[2]/div/div[1]/div[1]/div[2]/div[1]/div[1]/div
-    //*[@id="listing-id"]/div/div[2]/div/div[2]/div[1]/div[2]/div[1]/div[1]/div
-    await page.screenshot({ path: 'screenshotFrom2.png' });
-    // await page.getByText('DEL', { exact: true }).click();
-    // await page.getByPlaceholder('To').fill('BOM')
-    // await page.screenshot({ path: 'screenshotend.png' });
+    });
 
 });
-
 
